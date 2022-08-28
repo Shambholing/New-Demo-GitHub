5e740a2b596d010b71be34e6c3b03542783ec7cc
BIDAVE <BIDAVE@DESKTOP-I5N3H0C>
2022-08-28 13:15:50 +0530

    [ two classes are created name One , two

---
+++ src/Sample_new1/Demo_Two.java
@@ -0,0 +1,14 @@
+package Sample_new1;
+
+public class Demo_Two 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println(" Ankush Hundekar");
+		
+		System.out.println("Parmeshwar Bidve");
+		
+	}
+}
---
+++ src/Sample_new1/Demo_one.java
@@ -0,0 +1,13 @@
+package Sample_new1;
+
+public class Demo_one 
+{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ganesh Bidve");
+		
+		System.out.println("Ganesh Attarge");
+		
+	}
+}
