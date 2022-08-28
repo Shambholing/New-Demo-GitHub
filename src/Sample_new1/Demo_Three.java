b658bd6fbb19f4e7d8d77cf39aa3d4999389f229
BIDAVE <BIDAVE@DESKTOP-I5N3H0C>
2022-08-28 13:19:02 +0530

    [One class is add in old two classes

---
+++ src/Sample_new1/Demo_Three.java
@@ -0,0 +1,14 @@
+package Sample_new1;
+
+public class Demo_Three 
+{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Shambholing Bidve");
+		
+		
+		System.out.println("Vaijanath Bhange");
+		
+	}
+}
